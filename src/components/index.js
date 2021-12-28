46b0786a6e836809d5e3083c860a71f97ef30ea1
bidhanroyjoy <bidhan19994@gmail.com>
2021-12-28 23:41:42 +0600

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,20 @@
+import Details from './Details/Details';
+import Main from './Main/Main';
+import Snackbar from './Snackbar/Snackbar';
+import InfoCard from './InfoCard';
+import Form from './Main/Form/Form';
+
+
+export{
+    Details,
+    Main,
+    Form,
+    Snackbar,
+    InfoCard,
+}  
+
+
+// export { default as Details } from './Details/Details';
+// export { default as Main } from './Main/Main';
+// export { default as Snackbar } from './Snackbar/Snackbar';
+// export { default as InfoCard } from './InfoCard';
